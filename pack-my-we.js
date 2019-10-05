14bed3e4ca56475932232239fcf5ea48a19405ab
lironh <lironenk@gmail.com>
2019-10-05 09:55:15 +0300

    refactor: renamed script

--- pack-my-we.js
+++ pack-my-we.js
@@ -7,5 +7,5 @@ const concat = require("concat");
     "./dist/s1-angular-elements-demo/vendor-es2015.js",
     "./dist/s1-angular-elements-demo/styles-es2015.js"
   ];
-  await concat(files, "./simple-dist/packed-wc.js");
+  await concat(files, "./simple-dist/silly-btn.js");
 })();
